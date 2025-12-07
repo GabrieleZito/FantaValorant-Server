69997b0ca2f73ea092198cf922474a9da1281585
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-12-07 14:52:37 +0100

    feat: change to local sqlite db

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,32 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    },
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "../fantavalorant.sqlite", 
     logging: false,
 });
-
-/* const sequelize = new Sequelize({
-    dialect: "postgres",
-    database: "postgres",
-    username: "postgres",
-    password: "1231231231",
-    host: "localhost",
-    port: 5432,
-    logging: false,
-}); */
-
-/* console.log("Database name:", sequelize.getDatabaseName());
-console.log("Connection config:", {
-    host: sequelize.config.host,
-    port: sequelize.config.port,
-    database: sequelize.config.database,
-    username: sequelize.config.username,
-}); */
 
 module.exports = sequelize;
