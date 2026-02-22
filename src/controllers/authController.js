63360d34737cbc07fe7e0e8599966f78cdc33078
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2026-02-22 17:46:05 +0100

    fix: update SameSite attribute for refreshToken cookie based on environment

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -52,7 +52,7 @@ const registerUser = async (req, res) => {
             res.cookie("refreshToken", refreshToken, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
-                sameSite: "strict",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
                 maxAge: 7 * 24 * 60 * 60 * 1000,
             });
             res.status(200).json({
