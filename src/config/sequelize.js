188cf13cb10f5a94cf0b779846e1738b270c46dd
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2026-02-22 16:24:31 +0100

    feat: add PostgreSQL support and update dependencies

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,11 +3,23 @@ import { config } from "dotenv";
 config();
 
 // @ts-ignore
-const sequelize = new Sequelize("fantavalorant", process.env.DATABASE_USER, process.env.DATABASE_PASS, {
+/* const sequelize = new Sequelize("fantavalorant", process.env.DATABASE_USER, process.env.DATABASE_PASS, {
     host: process.env.DATABASE_URL,
     dialect: "mysql",
     logging: false,
     port: process.env.DATABASE_PORT,
+}); */
+
+// @ts-ignore
+const sequelize = new Sequelize(process.env.DATABASE_URL , {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // required for Neon
+        },
+    },
 });
 
 export default sequelize;
